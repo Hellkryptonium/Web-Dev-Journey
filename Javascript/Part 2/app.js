2f683fe2f927baadc6fb70e687fcb7340d25eb6b
Mohd Harish <163094451+Hellkryptonium@users.noreply.github.com>
2025-09-27 17:28:52 +0530

    Add pricing page layout and JavaScript examples for Parts 2 and 3

---
+++ Javascript/Part 2/app.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 3000;
+let c = 20;
+let num1 = 20;
+let num2 = 30;
+
+let lastDigit1 = num1%10;
+let lastdigit2 = num2%10;
+if(lastDigit1 === lastdigit2) {
+    console.log("yes");
+} else {
+    console.log("no");
+}
---
+++ Javascript/Part 3/app.js
@@ -0,0 +1,7 @@
+let str = 'A';
+
+if(str === str.toLowerCase()) {
+    console.log("yes");
+} else {
+    console.log("no");
+}
