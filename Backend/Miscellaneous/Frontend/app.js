14e68b281be78c40a3e79966ddaad3180d8dcb1a
Mohd Harish <163094451+Hellkryptonium@users.noreply.github.com>
2026-01-09 21:34:30 +0530

    feat: Initialize backend and frontend structure
    - Added package.json for backend with express and nodemon dependencies.
    - Created app.js for frontend with Person, Student, and Teacher classes.
    - Developed index.html with forms for GET and POST requests to the backend.

---
+++ Backend/Miscellaneous/Frontend/app.js
@@ -0,0 +1,30 @@
+class Person {
+    constructor(name, age) {
+        console.log("person class constructor");
+        this.name = name;
+        this.age = age;
+    }
+    talk() {
+        console.log(`Hi, I am ${this.name}`);
+    }
+}
+
+class Student extends Person{
+    constructor(name, age, marks) {
+        console.log("student class constructor")
+        super(name, age);
+        this.marks = marks;
+    }
+    
+}
+
+let stu1 = new Student("adam", 25, 95);
+
+class Teacher extends Person{
+    constructor(name, age, subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+  
+}
+
